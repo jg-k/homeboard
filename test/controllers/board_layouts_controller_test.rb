6a702a5bba736dd00e544d4f774c0e5ae4818d5e
Jean-Gregoire Kherian <jgkherian@gmail.com>
2026-08-18 14:56:32 +0100

    rotate default imahe

--- test/controllers/board_layouts_controller_test.rb
+++ test/controllers/board_layouts_controller_test.rb
@@ -15,6 +15,20 @@ class BoardLayoutsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to board_path(@board)
   end
 
+  # The sample photo was stored sideways; a board layout has to be portrait or
+  # the hold markers land in the wrong place.
+  test "the sample image is attached the right way up" do
+    post board_board_layouts_url(@board), params: { board_layout: { name: "Sample Layout", use_sample_image: "1" } }
+
+    attached = BoardLayout.order(:id).last.image_layout
+    assert attached.attached?
+
+    attached.blob.analyze
+    metadata = attached.blob.reload.metadata
+    assert_operator metadata["height"], :>, metadata["width"],
+      "expected the sample layout image to be portrait"
+  end
+
   test "should update board layout" do
     patch board_board_layout_url(@board, @board_layout), params: { board_layout: { name: "Updated Layout" } }
     assert_redirected_to board_path(@board)
